07b0be98f7f39fdbc741519a93733b73a4f07850
jh8w8m <36068285+jh8w8m@users.noreply.github.com>
2021-10-26 00:26:41 +0800

    remain one render area

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import "./App.scss";
 import Stats from 'three/examples/jsm/libs/stats.module';
 import Video from './Video';
 import Counter from './Counter';
+import rhythm from './rhythm.json';
+import TWEEN from '@tweenjs/tween.js';
 
 // function App() {
 //     const mount = useRef<HTMLDivElement>(null)
@@ -200,19 +202,323 @@ import Counter from './Counter';
 // }
 
 
+
+// const App = () => {
+//     const mount = useRef<HTMLDivElement>(null)
+//     const controls = useRef<sceneRef | null>(null);
+//     const [ score, setScore ] = useState(0);
+    
+    
+
+//     useEffect(() => {
+//         if (!mount.current) {
+//             throw Error("null ref");
+//         }
+//         else {
+//             var axes = new AxesHelper(20);
+//             let width = mount.current.clientWidth
+//             let height = mount.current.clientHeight
+//             let stats = Stats();
+//             const scene = new Scene()
+            
+
+//             const camera = new PerspectiveCamera(75, width / height, 0.1, 1000)
+//             // const orbitControls = new OrbitControls(camera);
+//             // orbitControls.autoRotate = false;
+//             const renderer = new WebGLRenderer({ antialias: true, alpha:true })
+
+            
+//             // 创建一个网格对象
+//             const geometry = new BoxGeometry(0.8, 0.1, 12) // 盒体的长宽高
+//             // 判定区域 z轴 8.8 ~ 10
+//             const material = new MeshBasicMaterial({ color: 0xff00ff })
+//             //const cube = new Mesh(geometry, material)
+            
+
+//             const rail1 = new Mesh(geometry, material);
+//             const rail2 = new Mesh(geometry, material);
+//             const rail3 = new Mesh(geometry, material);
+//             rail1.translateX(1);
+//             rail3.translateX(-1);
+//             rail1.rotateX(0.05); // 内部为弧度，弧度计算 (角度/180)*PAI = 弧度
+//             rail2.rotateX(0.05);
+//             rail3.rotateX(0.05);
+//             rail1.translateZ(5);
+//             rail2.translateZ(5);
+//             rail3.translateZ(5);
+
+
+//             //scene.add(cube)
+//             scene.add(rail1);
+//             scene.add(rail2);
+//             scene.add(rail3);
+//             // 设置背景为透明色
+//             renderer.setClearColor('0xEEEEEE', 0.0)
+//             renderer.setSize(width, height)
+            
+//             // const point = new PointLight(0xFFFFFF);
+//             // point.position.set(0, 2, 0);
+//             // scene.add(point);
+//             scene.add(axes);
+            
+//             camera.position.x = 0;
+//             camera.position.y = 1;
+//             camera.position.z = 12.5;
+//             // camera.position.x = 3;
+//             // camera.position.y = -0.5;
+//             // camera.position.z = 11;
+//             // camera.rotateY(20);
+
+//             // 创建 downBlock 的 group
+//             let group = new Group();
+
+//             controls.current = { scene, renderer, camera, group, stats}; // 获取scene的引用
+
+//             const render = () => {
+//                 renderer.render(scene, camera);
+//                 stats.update();
+//             }
+            
+//             // window.size 改变，重新渲染图形
+//             const handleResize = () => {
+//                 if (!mount.current) {
+//                     throw Error("null mount ref");
+//                 }
+//                 width = mount.current.clientWidth
+//                 height = mount.current.clientHeight
+//                 renderer.setSize(width, height)
+//                 camera.aspect = width / height
+//                 camera.updateProjectionMatrix()
+//                 render()
+//             }
+                    
+//             mount.current.appendChild(renderer.domElement);
+//             mount.current.appendChild( stats.dom );
+//             window.addEventListener('resize', handleResize);
+//             //orbitControls.addEventListener('change', render);
+//             render();
+//             // stats.begin();
+
+//             const handleSpaceDown = (e:KeyboardEvent) => {
+//                 if(e.key == 'Space') {
+
+//                 }
+//             }
+
+//             const setKeybordDown = () => {
+//                 window.addEventListener('keypress', handleSpaceDown);
+//             }
+//             setKeybordDown();
+
+
+
+//             return () => { // 清除副作用
+//                 let realMount = mount.current as HTMLDivElement;
+//                 window.removeEventListener('resize', handleResize);
+//                 window.removeEventListener('keydown', handleSpaceDown);
+//                 //orbitControls.removeEventListener('change', render);
+
+//                 realMount.removeChild(renderer.domElement);
+        
+//                 scene.remove(rail1);
+//                 scene.remove(rail2);
+//                 scene.remove(rail3);
+//                 geometry.dispose();
+//                 material.dispose();
+
+//                 // stats.end();
+//             }
+//         }
+
+        
+
+//     }, [])
+    
+//     // const render = () => {
+//     //     let {
+//     //         scene,
+//     //         renderer,
+//     //         camera,
+//     //         stats,
+//     //     } = controls.current as sceneRef;
+
+//     //     renderer.render(scene, camera);
+//     //     stats.update();
+
+//     //     let T = clock.getDelta();
+//     //     tick += T;
+//     //     timeS = timeS + T;
+//     //     console.log(tick);
+        
+//     //     if (timeS >= renderInterval) {
+//     //         renderer.render(scene, camera); //执行渲染操作
+//     //         mixer.update(T);
+//     //         //console.log("here");
+//     //         //TWEEN.update(T);
+//     //         timeS = 0;
+//     //         stats.update();
+//     //     }
+        
+//     //     requestAnimationFrame(render);     
+
+//     // }
+
+//     const handleGenerate = (direction: string) => {
+//         let {
+//             scene,
+//             renderer,
+//             camera,
+//             group,
+//             stats,
+//         } = controls.current as sceneRef;
+
+//         const FPS = 60;
+//         const renderInterval = 1 / FPS; 
+
+//         let blockStartPosition = 0;
+//         let downTimer = 30;
+
+//         switch(direction) {
+//             case 'l': blockStartPosition = -1; break;
+//             case 'm': blockStartPosition = 0; break;
+//             case 'r': blockStartPosition = 1; break;
+//             default: throw new Error("wrong direction");
+//         }
+
+//         const geometry = new BoxGeometry(0.8, 0.03, 0.7) // 盒体的长宽高
+//         const material = new MeshBasicMaterial({ color: 0xDC143C })
+//         const block = new Mesh(geometry, material);
+//         // block.name = "downBox";
+//         // // block.translateX(1);
+//         // // block.translateY(0.5);
+//         // // block.rotateX(0.04);
+//         // //group.add(block);
+
+//         // 设置动画
+//         let times = [0, downTimer]; //关键帧时间数组，离散的时间点序列
+//         let values = [blockStartPosition, 0.06, 0, blockStartPosition, -0.5, 11]; //与时间点对应的值组成的数组
+//         // 创建位置关键帧对象：0时刻对应位置0, 0, 0   10时刻对应位置150, 0, 0
+//         let posTrack = new KeyframeTrack('downBox.position', times, values);
+
+//         // duration决定了默认的播放时间，一般取所有帧动画的最大时间
+//         // duration偏小，帧动画数据无法播放完，偏大，播放完帧动画会继续空播放
+//         var clip = new AnimationClip("default", downTimer, [posTrack])
+
+        
+
+//         //scene.add(group);
+//         scene.add(block);
+
+//         const handleAnimationFinished = () => {
+//             scene.remove(block);
+//         }
+
+//         // let tweenPosition = new TWEEN.Tween({x:blockStartPosition, y:0.06, z:0})
+//         // .to({ x: blockStartPosition, y: -0.5, z: 11 }, downTimer)
+//         // .easing(TWEEN.Easing.Linear.None)
+//         // .onComplete(handleAnimationFinished)
+//         // .start(0);
+
+//         // group作为混合器的参数，可以播放group中所有子对象的帧动画
+//         var mixer = new AnimationMixer(block);
+//         //var mixer = new AnimationMixer(group);
+//         // 剪辑clip作为参数，通过混合器clipAction方法返回一个操作对象AnimationAction
+//         var AnimationAction = mixer.clipAction(clip);
+//         //通过操作Action设置播放方式
+//         AnimationAction.timeScale = downTimer; //默认1，可以调节播放速度
+//         AnimationAction.loop = LoopOnce; //不循环播放
+//         AnimationAction.play(); //开始播放
+        
+//         mixer.addEventListener('finished', handleAnimationFinished);
+
+//         let clock = new Clock();
+//         let timeS = 0;
+//         // 渲染函数
+//         let tick = 0;
+//         function render() {
+
+//             let T = clock.getDelta();
+//             tick += T;
+//             timeS = timeS + T;
+//             console.log(tick);
+            
+//             if (timeS >= renderInterval) {
+//                 renderer.render(scene, camera); //执行渲染操作
+//                 mixer.update(T);
+//                 //console.log("here");
+//                 //TWEEN.update(T);
+//                 timeS = 0;
+//                 stats.update();
+//             }
+            
+//             requestAnimationFrame(render);            
+//         }
+//         render();
+//         // const render = () => {
+//         //     renderer.render(scene, camera);
+//         //     console.log("render 一次");
+//         // }
+//         // render();
+
+        
+//         // const animate = () => {
+//         //     cube.rotation.x += 0.01
+//         //     cube.rotation.y += 0.01
+    
+//         //     render()
+//         //     frameId = window.requestAnimationFrame(animate)
+//         // }
+
+//         // const start = () => {
+//         //     if (!frameId) {
+//         //     frameId = requestAnimationFrame(animate)
+//         //     }
+//         // }
+    
+//         // const stop = () => {
+//         //     cancelAnimationFrame(frameId as number)
+//         //     frameId = null
+//         // }
+//     }
+//     // useEffect(() => {
+//     //     if (isAnimating && controls.current) {
+//     //         (controls.current).start()
+//     //     } else if (!isAnimating && controls.current){
+//     //         controls.current.stop()
+//     //     }
+//     // }, [isAnimating])
+    
+//     return(
+//         <>
+//             <div className="vis" ref={mount}/>
+//             <Video src={require('./test4.mp4').default}  autoPlay={false} loop={true}/>
+//             <Counter counts={score}></Counter>
+//             <button className="animateButton animateButtonLeft" onClick={() => {handleGenerate('l')}}>动画左</button>
+//             <button className="animateButton animateButtonMid" onClick={() => {handleGenerate('m')}}>动画中</button>
+//             <button className="animateButton animateButtonRight" onClick={() => {handleGenerate('r')}}>动画右</button>
+//         </>
+//     ) 
+// }
+
+
 type sceneRef = {
     scene: Scene,
     renderer: Renderer,
     camera: Camera,
     group: Group,
+    stats: Stats,
+    mixer: AnimationMixer,
 }
+
+
 
 const App = () => {
     const mount = useRef<HTMLDivElement>(null)
-    const [isAnimating, setAnimating] = useState(true)
     const controls = useRef<sceneRef | null>(null);
     const [ score, setScore ] = useState(0);
     
+    
+
     useEffect(() => {
         if (!mount.current) {
             throw Error("null ref");
@@ -275,12 +581,9 @@ const App = () => {
             // 创建 downBlock 的 group
             let group = new Group();
 
-            controls.current = { scene, renderer, camera, group}; // 获取scene的引用
+            
 
-            const render = () => {
-                renderer.render(scene, camera);
-                console.log("render 一次");
-            }
+            
             
             // window.size 改变，重新渲染图形
             const handleResize = () => {
@@ -299,12 +602,53 @@ const App = () => {
             mount.current.appendChild( stats.dom );
             window.addEventListener('resize', handleResize);
             //orbitControls.addEventListener('change', render);
-            render();
-            stats.update();
             
+            // stats.begin();
+
+            const handleSpaceDown = (e:KeyboardEvent) => {
+                if(e.key == 'Space') {
+
+                }
+            }
+
+            const setKeybordDown = () => {
+                window.addEventListener('keypress', handleSpaceDown);
+            }
+            setKeybordDown();
+
+            // group作为混合器的参数，可以播放group中所有子对象的帧动画
+            scene.add(group);
+            var mixer = new AnimationMixer(group);
+            
+
+            controls.current = { scene, renderer, camera, group, stats, mixer}; // 获取scene的引用
+            let clock = new Clock();
+            let timeS = 0;
+            let tick = 0;
+            let FPS = 100;
+            let renderInterval = 1 / FPS;
+
+            const render = () => {
+                let T = clock.getDelta();
+                tick += T;
+                timeS = timeS + T;
+                if (timeS >= renderInterval) {
+                    renderer.render(scene, camera); //执行渲染操作
+                    mixer.update(T);
+                    //console.log("here");
+                    //TWEEN.update(T);
+                    timeS = 0;
+                    stats.update();
+                }
+                requestAnimationFrame(render);    
+            }
+            
+            render();
+
             return () => { // 清除副作用
                 let realMount = mount.current as HTMLDivElement;
                 window.removeEventListener('resize', handleResize);
+                window.removeEventListener('keydown', handleSpaceDown);
                 //orbitControls.removeEventListener('change', render);
 
                 realMount.removeChild(renderer.domElement);
@@ -314,6 +658,8 @@ const App = () => {
                 scene.remove(rail3);
                 geometry.dispose();
                 material.dispose();
+
+                // stats.end();
             }
         }
 
@@ -321,13 +667,45 @@ const App = () => {
 
     }, [])
     
+    // const render = () => {
+    //     let {
+    //         scene,
+    //         renderer,
+    //         camera,
+    //         stats,
+    //     } = controls.current as sceneRef;
+
+    //     renderer.render(scene, camera);
+    //     stats.update();
+
+    //     let T = clock.getDelta();
+    //     tick += T;
+    //     timeS = timeS + T;
+    //     console.log(tick);
+        
+    //     if (timeS >= renderInterval) {
+    //         renderer.render(scene, camera); //执行渲染操作
+    //         mixer.update(T);
+    //         //console.log("here");
+    //         //TWEEN.update(T);
+    //         timeS = 0;
+    //         stats.update();
+    //     }
+        
+    //     requestAnimationFrame(render);     
+
+    // }
+
     const handleGenerate = (direction: string) => {
         let {
             scene,
             renderer,
             camera,
             group,
+            stats,
+            mixer,
         } = controls.current as sceneRef;
+
 
         let blockStartPosition = 0;
         let downTimer = 30;
@@ -338,16 +716,18 @@ const App = () => {
             case 'r': blockStartPosition = 1; break;
             default: throw new Error("wrong direction");
         }
+        
 
         const geometry = new BoxGeometry(0.8, 0.03, 0.7) // 盒体的长宽高
         const material = new MeshBasicMaterial({ color: 0xDC143C })
         const block = new Mesh(geometry, material);
-        block.name = "downBox";
-        // block.translateX(1);
-        // block.translateY(0.5);
-        // block.rotateX(0.04);
-        //group.add(block);
+        // block.name = "downBox";
+        // // block.translateX(1);
+        // // block.translateY(0.5);
+        // // block.rotateX(0.04);
+        // //group.add(block);
 
+        // 设置动画
         let times = [0, downTimer]; //关键帧时间数组，离散的时间点序列
         let values = [blockStartPosition, 0.06, 0, blockStartPosition, -0.5, 11]; //与时间点对应的值组成的数组
         // 创建位置关键帧对象：0时刻对应位置0, 0, 0   10时刻对应位置150, 0, 0
@@ -355,34 +735,26 @@ const App = () => {
 
         // duration决定了默认的播放时间，一般取所有帧动画的最大时间
         // duration偏小，帧动画数据无法播放完，偏大，播放完帧动画会继续空播放
-        let duration = 20;
-        var clip = new AnimationClip("default", 30, [posTrack])
-
+        var clip = new AnimationClip("default", downTimer, [posTrack])
+        group.add(block);
+        //console.log(group.children)
+        
         //scene.add(group);
-        scene.add(block);
 
-        // group作为混合器的参数，可以播放group中所有子对象的帧动画
-        var mixer = new AnimationMixer(block);
+        // const handleAnimationFinished = () => {
+        //     group.remove(block);
+        // }
+
         //var mixer = new AnimationMixer(group);
         // 剪辑clip作为参数，通过混合器clipAction方法返回一个操作对象AnimationAction
         var AnimationAction = mixer.clipAction(clip);
         //通过操作Action设置播放方式
-        AnimationAction.timeScale = 20; //默认1，可以调节播放速度
+        AnimationAction.timeScale = downTimer; //默认1，可以调节播放速度
         AnimationAction.loop = LoopOnce; //不循环播放
         AnimationAction.play(); //开始播放
         
+        // mixer.addEventListener('finished', handleAnimationFinished);
 
-        var clock = new Clock();
-        // 渲染函数
-        function render() {
-            renderer.render(scene, camera); //执行渲染操作
-            requestAnimationFrame(render); //请求再次执行渲染函数render，渲染下一帧
-
-            //clock.getDelta()方法获得两帧的时间间隔
-            // 更新混合器相关的时间
-            mixer.update(clock.getDelta());
-        }
-        render();
         // const render = () => {
         //     renderer.render(scene, camera);
         //     console.log("render 一次");
@@ -419,7 +791,7 @@ const App = () => {
     
     return(
         <>
-            <div className="vis" ref={mount} onClick={() => setAnimating(!isAnimating)} />
+            <div className="vis" ref={mount}/>
             <Video src={require('./test4.mp4').default}  autoPlay={false} loop={true}/>
             <Counter counts={score}></Counter>
             <button className="animateButton animateButtonLeft" onClick={() => {handleGenerate('l')}}>动画左</button>
@@ -428,6 +800,7 @@ const App = () => {
         </>
     ) 
 }
+
 
 
 export default App;
